7210ba93ab07f98942fd5d21bc1076f46ed6432e
formindev <formindeve@gmail.com>
2019-11-14 04:51:33 +0400

    Added BlackJack class, added new choice

---
+++ src/main/java/games/BlackJack.java
@@ -0,0 +1,7 @@
+package games;
+
+public class BlackJack {
+  public static void main(final String... args) {
+    System.out.println("Hello World!");
+  }
+}
--- src/main/java/games/Choice.java
+++ src/main/java/games/Choice.java
@@ -3,14 +3,33 @@ package games;
 import java.io.IOException;
 
 public class Choice {
+  private static final String LINE_SEPARATOR = System.lineSeparator();
+
   public static void main(final String... args) throws IOException {
-    System.out.println("Выберите игру:\n1 - \"однорукий бандит\", 2 - \"пьяница\"");
-    switch (System.in.read()) {
+    System.out.println("Выберите игру:\n1 - \"однорукий бандит\", 2 - \"пьяница\", 3 - \"очко\"");
+    switch (getCharacterFromUser()) {
       case '1': Slot.main();
               break;
       case '2': Drunkard.main();
               break;
+      case '3': BlackJack.main();
+              break;
       default: System.out.println("Нет игры с таким номером!");
     }
+  }
+
+  private static char getCharacterFromUser() throws IOException {
+    byte[] input = new byte[1 + getSeparatorLength()];
+    if (System.in.read(input) != input.length)
+      throw new RuntimeException("Пользователь ввёл недостаточное кол-во символов");
+    return (char) input[0];
+  }
+
+  private static int getSeparatorLength() {
+    final String osName = System.getProperty("os.name");
+    if (osName.toUpperCase().contains("WIN"))
+      return LINE_SEPARATOR.length() - 1;
+    else
+      return LINE_SEPARATOR.length();
   }
 }
